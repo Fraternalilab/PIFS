97aa628a54da099c1eb3df83c38f45843d3eb44d
josef0731 <joseph.chifung.ng@gmail.com>
2019-11-27 11:39:58 +0000

    cleanup

--- pifs_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python -u pifs.py --structList test2 --inDir ../pdb/20180101/ --networkOutDir dna_network/ --popscompOutDir ../pdbInterfaceScreen/popscomp/ --pifsDir dna_pifs/ --pifsTag dna --popsDir ../ --popscompDir ../POPSCOMP/ --errorFile /data/home/josephn/pifs/test2.err --distCutoff 6.0
-
--- pifs_submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#£type=rna
-#sbatch --nodelist=beo-06 --export=file=`echo $type`,networkDir=`echo $type`_network/,pifsDir=`echo $type`_pifs/,pifsTag=`echo $type` --mem-per-cpu=4G pifs.sh
-
-type=dna
-for id in {1..2}; do
-	sbatch --nodelist=beo-09 --export=file=`echo $type`_`echo $id`,networkDir=`echo $type`_network/,pifsDir=`echo $type`_pifs/,pifsTag=`echo $type` --mem-per-cpu=4G pifs.sh
-done
